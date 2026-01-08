79202f31ea4b6ad17b4fdcd6fb1d3b4a8b17a07f
gokselkaradag <goksel.karadag34@gmail.com>
2026-01-08 19:31:23 +0300

    complete discount microservice with dapper crud operations

---
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+
+namespace MultiShop.Discount.Context;
+
+public class DapperContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    private readonly string _connectionString;
+    
+    public DapperContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _connectionString = _configuration.GetConnectionString("DefaultConnection");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost,1433;Initial Catalog=MultiShopDiscountDb;User Id=sa;Password=Aaa...2026!;TrustServerCertificate=True");
+    }
+    
+    public DbSet<Coupon> Coupons  { get; set; }
+    public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+}
--- Services/Discount/MultiShop.Discount/Program.cs
+++ Services/Discount/MultiShop.Discount/Program.cs
@@ -1,7 +1,16 @@
+using MultiShop.Discount.Context;
+using MultiShop.Discount.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<DapperContext>();
+builder.Services.AddTransient<IDiscountService, DiscountService>();
+
+builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -9,33 +18,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
