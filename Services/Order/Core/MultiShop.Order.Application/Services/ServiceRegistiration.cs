47d49af5cd9cc7b67bb4cb18a7a684d95d1d09e7
gokselkaradag <goksel.karadag34@gmail.com>
2026-02-21 11:41:17 +0300

    microservice_mediatR_api&registiration

---
+++ Services/Order/Core/MultiShop.Order.Application/Services/ServiceRegistiration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+
+namespace MultiShop.Order.Application.Services;
+
+public static class ServiceRegistiration
+{
+    //Bu yapı ilgili kütüphanemizin Registiration işlemini gerçekleştiriyor, program.cs de her seferinde mediatR çağırmak zorunda değiliz.
+    
+    public static void AddApplicationService(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ServiceRegistiration).Assembly));
+    }
+}
--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,8 +1,18 @@
 using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Application.Services;
+using MultiShop.Order.Persistance.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//IRepository içinde Repository registiration işlemi.
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+//Configuraiton program.cs işleme.
+builder.Services.AddApplicationService(builder.Configuration);
+
+#region 
 builder.Services.AddScoped<GetAddressQueryHandler>();
 builder.Services.AddScoped<GetAddressByIdQueryHandler>();
 builder.Services.AddScoped<CreateAddressCommandHandler>();
@@ -14,6 +24,7 @@ builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
 builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
 builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
 builder.Services.AddScoped<DeleteOrderDetailCommandHandler>();
+#endregion
 
 // Add services to the container.
 builder.Services.AddControllers();
