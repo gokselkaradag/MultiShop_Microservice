ec3ba0431cee9dc89d1c84a1153b2d03dedb58cb
gokselkaradag <goksel.karadag34@gmail.com>
2026-02-27 13:02:11 +0300

    feat: add microservice cargo layer

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/EntityFramework/EfCargoCaompanyDal.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Concrete;
+using MultiShop.Cargo.DataAccessLayer.Repositories;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.DataAccessLayer.EntityFramework;
+
+public class EfCargoCaompanyDal : GenericRepository<CargoCompany>, ICargoCompanyDal
+{
+    public EfCargoCaompanyDal(CargoContext context) : base(context)
+    {
+    }
+}
