03000b638d52f8480cf266e7cdfa8ffff4240cf5
gokselkaradag <goksel.karadag34@gmail.com>
2026-02-28 11:42:48 +0300

    feat: add microserivce cargo business layer

---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Abstract/ICargoCustomerService.cs
@@ -0,0 +1,9 @@
+﻿using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.BusinessLayer.Abstract;
+
+public interface ICargoCustomerService : IGenericService<CargoCustomer>
+{
+    
+}
